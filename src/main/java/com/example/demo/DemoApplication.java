8e014c5e01c34ea1042ab629830875fc09ce0b78
nmadhab <nilmadhab1994@booking.com>
2021-02-07 13:17:11 +0000

    hello world working

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 //@ComponentScan(basePackages = )
 public class DemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
